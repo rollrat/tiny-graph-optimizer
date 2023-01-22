6c3e9b8d96e2505dc2f0149ee3d12578e3d1ea03
rollrat <rollrat.cse@gmail.com>
2023-01-22 15:20:17 +0900

    core(graph): declare graph, operator structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::rc::{Rc, Weak};
+
+struct MatrixDescription {
+    column_size: u32,
+    row_size: u32,
+}
+
+struct GraphNode {
+    desc: MatrixDescription,
+    parent: Weak<GraphOperator>,
+    childs: Vec<Rc<GraphOperator>>,
+}
+
+enum GraphOperation {
+    none,
+    plus,
+    minus,
+    multiple,
+    divide,
+}
+
+struct GraphOperator {
+    op: GraphOperation,
+    parents: Vec<Weak<GraphNode>>,
+    childs: Vec<Rc<GraphNode>>,
+}
+
+fn simulate_graph(node: GraphNode) {}
+
 fn main() {
     println!("Hello, world!");
 }
